2b48f8359414c55538eb7b2326901711bfdc19f6
NjakazanaBuda <njakazana.buda@gmail.com>
2018-02-25 22:31:45 +0200

    removing classes

--- moconsulting/src/main/java/com/moconsulting/enitty/Users.java
+++ moconsulting/src/main/java/com/moconsulting/enitty/Users.java
@@ -2,10 +2,16 @@ package com.moconsulting.enitty;
 
 import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 
+@Entity
+@Table(name = "Users")
 public class Users implements Serializable{
 
 }
