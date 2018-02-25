24abb813b27e9793529bd0b7025fc9c5aafdb00d
NjakazanaBuda <njakazana.buda@gmail.com>
2018-02-25 22:34:35 +0200

    Add files via upload

---
+++ moconsulting/src/main/java/com/moconsulting/enitty/CompanyInfo.java
@@ -0,0 +1,11 @@
+package com.moconsulting.enitty;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+
+public class CompanyInfo implements Serializable {
+
+}
