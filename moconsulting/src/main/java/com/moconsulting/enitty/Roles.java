c9f448ac99e87472ddab8d83375ed72da09b2658
NjakazanaBuda <njakazana.buda@gmail.com>
2018-02-25 23:06:20 +0200

    added variable and getters on roles class

--- moconsulting/src/main/java/com/moconsulting/enitty/Roles.java
+++ moconsulting/src/main/java/com/moconsulting/enitty/Roles.java
@@ -3,9 +3,40 @@ package com.moconsulting.enitty;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 
+@Entity
+@Table(name = "Roles")
 public class Roles implements Serializable{
+	
+	@Id
+	@Column(name = "role_id")
+	private int roleid;
+
+	@Column(name = "role_name")
+	private String rolename;
+	
+	public int getRoleid() {
+		return roleid;
+	}
+
+	public void setRoleid(int roleid) {
+		this.roleid = roleid;
+	}
+
+    public String getRolename() {
+		return rolename;
+	}
+
+	public void setRolename(String rolename) {
+		this.rolename = rolename;
+	}
+	
 
 }
